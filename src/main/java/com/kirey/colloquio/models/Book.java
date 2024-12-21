39a57a8f34bc24603f684285862f368a907cfd2c
Tommaso <mangiavacchitommaso@gmail.com>
2024-12-21 15:32:37 +0100

    creati i DTO e il domain

--- src/main/java/com/kirey/colloquio/models/Book.java
+++
@@ -1,5 +0,0 @@
-package com.kirey.colloquio.models;
-
-public class Book {
-
-}
---
+++ src/main/java/com/kirey/colloquio/models/BookOutputDTO.java
@@ -0,0 +1,15 @@
+package com.kirey.colloquio.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class BookOutputDTO {
+    private String id;
+    private String title;
+    private String author;
+    private String description;
+    private String publishDate;
+    private int price;
+}

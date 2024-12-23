ab2db17852186ed0a001ed0a561f00b2c66a0486
Tommaso <mangiavacchitommaso@gmail.com>
2024-12-23 13:29:40 +0100

    relaxed validation constraints

--- src/main/java/com/kirey/colloquio/domain/Book.java
+++ src/main/java/com/kirey/colloquio/domain/Book.java
@@ -14,19 +14,19 @@
     public class Book {
         @Id
         private String id;
-        @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+        @Size(max = 50, message = "Title cannot exceed 50 characters")
         @NotBlank(message = "Title cannot be blank")
         private String title;
-        @Size(max = 50, message = "Description cannot exceed 50 characters")
+        @Size(max = 50, message = "Author cannot exceed 50 characters")
         @NotBlank(message = "Author cannot be blank")
         private String author;
-        @Size(max = 100, message = "Description cannot exceed 100 characters")
+        @Size(max = 200, message = "Description cannot exceed 100 characters")
         @NotBlank(message = "Description cannot be blank")
         private String description;
         @Min(value = 0, message = "PublishYear must be at least 0")
         private int publishYear;
         @Min(value = 0, message = "Price must be at least 0")
-        @Max(value = 100, message = "Price cannot exceed 100")
+        @Max(value = 1000, message = "Price cannot exceed 100")
         private int price;
 
     }
